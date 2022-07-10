0ba1bffffc077b0430fac658c1c27467ba27847c
Sushmita Priya <56189077+Sushmita-23@users.noreply.github.com>
2022-07-10 21:23:13 +0530

    Path Visualizer

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PathVis from './PathVis/PathVis';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <PathVis></PathVis>
+    </div>
+  );
+}
+
+export default App;
